be29de90274e3bc2068c43b6cc597244d8d03623
Ray Rodriguez <rayrod2030@gmail.com>
2015-02-28 09:12:57 -0500

    Initial commit of empty cookbook

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+# rubocop:disable Style/SingleSpaceBeforeFirstArg
+name             'prometheus'
+maintainer       'Ray Rodriguez'
+maintainer_email 'rayrod2030@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures Prometheus'
+long_description 'Installs/Configures prometheus'
+version          '0.1.0'
+# rubocop:enable Style/SingleSpaceBeforeFirstArg
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: prometheus
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
