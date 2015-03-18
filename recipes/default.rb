a73267ce0e2d87507457c3cbadf2ba73f92d6608
Eric Richardson <e@ericrichardson.com>
2015-03-18 08:44:00 -0400

    Remove extra colon in local prometheus job target

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,7 +34,7 @@ end
 # monitor our server instance
 prometheus_job "prometheus" do
   scrape_interval   "15s"
-  target            "http://localhost:#{node['prometheus']['flags']['web.listen-address']}#{node['prometheus']['flags']['web.telemetry-path']}"
+  target            "http://localhost#{node['prometheus']['flags']['web.listen-address']}#{node['prometheus']['flags']['web.telemetry-path']}"
 end
 
 accumulator node['prometheus']['flags']['config.file'] do
