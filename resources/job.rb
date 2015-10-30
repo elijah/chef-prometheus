fe1c4d4fb8ff418b9a17ae8227accf07551f4c24
Francois Visconte <f.visconte@criteo.com>
2015-10-30 15:35:46 +0100

    Add support for consul

--- resources/job.rb
+++ resources/job.rb
@@ -6,3 +6,5 @@ attribute :scrape_interval,     kind_of: String
 attribute :scrape_timeout,      kind_of: String
 attribute :target,              kind_of: [Array, String]
 attribute :metrics_path,        kind_of: String, default: '/metrics'
+attribute :consul_sd_server,    kind_of: String, default: nil
+attribute :consul_sd_services,  kind_of: [Array, String]
