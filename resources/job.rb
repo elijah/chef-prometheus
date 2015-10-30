6f45c096810bc4f4732f97512a8e9894c35ad36b
Francois Visconte <f.visconte@criteo.com>
2015-10-30 17:23:03 +0100

    Add consul datacenter support

--- resources/job.rb
+++ resources/job.rb
@@ -1,10 +1,11 @@
 default_action :create
 actions :delete
 
-attribute :name,                kind_of: String, name_attribute: true, required: true
-attribute :scrape_interval,     kind_of: String
-attribute :scrape_timeout,      kind_of: String
-attribute :target,              kind_of: [Array, String]
-attribute :metrics_path,        kind_of: String, default: '/metrics'
-attribute :consul_sd_server,    kind_of: String, default: nil
-attribute :consul_sd_services,  kind_of: [Array, String]
+attribute :name,                 kind_of: String, name_attribute: true, required: true
+attribute :scrape_interval,      kind_of: String
+attribute :scrape_timeout,       kind_of: String
+attribute :target,               kind_of: [Array, String]
+attribute :metrics_path,         kind_of: String, default: '/metrics'
+attribute :consul_sd_server,     kind_of: String, default: nil
+attribute :consul_sd_services,   kind_of: [Array, String]
+attribute :consul_sd_datacenter, kind_of: String, default: nil
