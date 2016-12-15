a4ae06641a7585f66352697e42cf61748c3a94aa
Elijah Wright <elijah.wright@gmail.com>
2016-12-15 15:59:18 -0600

    Merge several version updates and changes from @fjaeckel.

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 # rubocop:disable Style/SingleSpaceBeforeFirstArg
 name             'prometheus'
-maintainer       'Ray Rodriguez'
-maintainer_email 'rayrod2030@gmail.com'
+maintainer       'Elijah Wright'
+maintainer_email 'elijah.wright@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures Prometheus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.1'
+version          '0.6.2'
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
 
 %w( ubuntu debian centos redhat fedora ).each do |os|
