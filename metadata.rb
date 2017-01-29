2cdd1d54bb2854c2cbec6c793b002b70c2de9818
servak <fservak@gmail.com>
2017-01-30 01:27:35 +0900

    fixed source_url, issues_url

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures Prometheus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.6.3'
-source_url 'https://github.com/servak/chef-prometheus'
-issues_url 'https://github.com/servak/chef-prometheus/issues'
+source_url 'https://github.com/elijah/chef-prometheus'
+issues_url 'https://github.com/elijah/chef-prometheus/issues'
 
 %w(ubuntu debian centos redhat fedora).each do |os|
   supports os
